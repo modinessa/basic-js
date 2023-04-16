190de35f95a21e278f0490b77c9ae6bb344011d0
Inessa <modina.inessa@gmail.com>
2023-04-15 23:07:31 -0700

    simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,62 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: "",
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain !== "") {
+      return this.chain.split("~~").length;
+    } else {
+      return 0;
+    }
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value = " ") {
+    if (this.chain !== "") {
+      this.chain = this.chain + `~~( ${value} )`;
+    } else {
+      this.chain = `( ${value} )`;
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (
+      position < 1 ||
+      position > this.chain.split("~~").length - 1 ||
+      typeof position !== "number"
+    ) {
+      this.chain = "";
+      throw new Error("You can't remove incorrect link!");
+    } else {
+      const updatedChain = [];
+      this.chain.split("~~").forEach((link, i) => {
+        if (i + 1 !== position) {
+          updatedChain.push(link);
+        }
+      });
+      this.chain = updatedChain.join("~~");
+      return this;
+    }
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const reversedChain = this.chain.split("~~").reverse().join("~~");
+    this.chain = reversedChain;
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const finishedChain = this.chain;
+    this.chain = "";
+    return finishedChain;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };

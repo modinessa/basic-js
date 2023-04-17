437b4496762820870a42af066830bbcc4c8abefe
Inessa <modina.inessa@gmail.com>
2023-04-16 19:35:43 -0700

    transform-array, videnere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,70 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr.length == 0) {
+    return [];
+  }
+
+  let discardNext = false;
+  let doubleNext = false;
+  let noPrev = false;
+
+  return arr.reduce((newArray, val) => {
+    if (discardNext) {
+      discardNext = false;
+      noPrev = true;
+      return newArray;
+    }
+
+    switch (val) {
+      case "--discard-next":
+        discardNext = true;
+        noPrev = false;
+        return newArray;
+      case "--discard-prev":
+        if (!noPrev && newArray.length > 0) {
+          newArray.pop();
+        }
+        return newArray;
+      case "--double-next":
+        doubleNext = true;
+        noPrev = false;
+        return newArray;
+      case "--double-prev":
+        if (!noPrev && newArray.length > 0) {
+          newArray.push(newArray[newArray.length - 1]);
+        }
+        noPrev = false;
+        return newArray;
+      default:
+        newArray.push(val);
+        if (doubleNext) {
+          newArray.push(val);
+          doubleNext = false;
+        }
+        noPrev = false;
+        return newArray;
+    }
+  }, []);
 }
 
 module.exports = {
-  transform
+  transform,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,97 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  direct = true;
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  checkInput(message, key) {
+    if (!message || !key) {
+      throw Error("Incorrect arguments!");
+    }
+  }
+
+  encrypt(message, key) {
+    this.checkInput(message, key);
+
+    let index = -1;
+    return message.split("").reduce((str, letter) => {
+      const value = this.getValue(letter);
+      if (value == null) {
+        return this.combine(str, letter);
+      }
+
+      index++;
+      const keyValue = this.getValue(key.charAt(index % key.length));
+      return this.combine(str, this.getEnctyptValue(value, keyValue));
+    }, "");
+  }
+
+  decrypt(message, key) {
+    this.checkInput(message, key);
+
+    let index = -1;
+    return message.split("").reduce((str, letter) => {
+      const value = this.getValue(letter);
+      if (value == null) {
+        return this.combine(str, letter);
+      }
+
+      index++;
+      const keyValue = this.getValue(key.charAt(index % key.length));
+      return this.combine(str, this.getDectyptValue(value, keyValue));
+    }, "");
+  }
+
+  getEnctyptValue(value, keyValue) {
+    return String.fromCharCode(((value + keyValue) % 26) + 65);
+  }
+
+  getDectyptValue(value, keyValue) {
+    let diff = value - keyValue;
+    return String.fromCharCode((diff < 0 ? diff + 26 : diff) + 65);
+  }
+
+  getValue(char) {
+    const code = char.charCodeAt(0);
+    if (code < 65) {
+      return null;
+    } else if (code < 91) {
+      return code - 65;
+    } else if (code < 97) {
+      return null;
+    } else if (code < 123) {
+      return code - 97;
+    }
+    return null;
+  }
+
+  combine(str, letter) {
+    return this.direct ? str + letter : letter + str;
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };

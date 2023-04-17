1f813a5b66ac76bd6bbc079d9211852059e15f8b
Inessa <modina.inessa@gmail.com>
2023-04-17 16:47:19 -0700

    sum-digits, sort

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let list = [];
+  const final = [];
+
+  arr.forEach((el) => {
+    if (el != -1) {
+      list.push(el);
+    }
+  });
+  list = list.sort((a, b) => a - b);
+  for (let i = 0, j = 0; i < arr.length; i++) {
+    if (arr[i] != -1) {
+      final.push(list[j]);
+      j++;
+    } else {
+      final.push(-1);
+    }
+  }
+  return final;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -10,13 +10,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * For 100, the result should be 1 (1 + 0 + 0 = 1)
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
+ * 
+
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  return ((n - 1) % 9) + 1;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
